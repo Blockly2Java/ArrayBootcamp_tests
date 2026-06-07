c4028fee5330bfc96a125462cb7e8f7db2b17406
Valentin Herrmann <accounts@valentin-herrmann.com>
2026-06-08 00:00:44 +0200

    MainWrapper

--- test/b2j/wrappers/MainWrapper.java
+++ test/b2j/wrappers/MainWrapper.java
@@ -6,6 +6,13 @@ import io.github.valentinherrmann.levenshtein.MethodWrapper;
 public class MainWrapper<T> extends ClassWrapper<T>
 {
     private final MethodWrapper<T, ?> main;
+    private final MethodWrapper<T, int[]> aufgabe1;
+    private final MethodWrapper<T, double[]> aufgabe2;
+    private final MethodWrapper<T, String> aufgabe3;
+    private final MethodWrapper<T, Integer> aufgabe4;
+    private final MethodWrapper<T, Void> aufgabe5;
+    private final MethodWrapper<T, int[]> aufgabe6;
+    private final MethodWrapper<T, Integer> aufgabe7;
 
     public MainWrapper()
     {
@@ -22,6 +29,67 @@ public class MainWrapper<T> extends ClassWrapper<T>
                 "public",
                 "static"
             );
+
+        aufgabe1 = new MethodWrapper<>(
+                this,
+                "aufgabe1",
+                int[].class,
+                "public",
+                "static"
+            );
+
+        aufgabe2 = new MethodWrapper<>(
+                this,
+                "aufgabe2",
+                double[].class,
+                "public",
+                "static"
+            );
+
+        aufgabe3 = new MethodWrapper<>(
+                this,
+                "aufgabe3",
+                String.class,
+                "public",
+                "static"
+            );
+        aufgabe3.addParameter("String[]", "arr");
+
+        aufgabe4 = new MethodWrapper<>(
+                this,
+                "aufgabe4",
+                int.class,
+                "public",
+                "static"
+            );
+        aufgabe4.addParameter("int[]", "arr");
+
+        aufgabe5 = new MethodWrapper<>(
+                this,
+                "aufgabe5",
+                void.class,
+                "public",
+                "static"
+            );
+        aufgabe5.addParameter("String[]", "arr");
+
+        aufgabe6 = new MethodWrapper<>(
+                this,
+                "aufgabe6",
+                int[].class,
+                "public",
+                "static"
+            );
+        aufgabe6.addParameter("int[]", "arr");
+
+        aufgabe7 = new MethodWrapper<>(
+                this,
+                "aufgabe7",
+                int.class,
+                "public",
+                "static"
+            );
+        aufgabe7.addParameter("int[]", "arr");
         
     }
 
@@ -36,5 +104,33 @@ public class MainWrapper<T> extends ClassWrapper<T>
 
     public MethodWrapper<T, ?> main() {
         return main;
+    }
+
+    public MethodWrapper<T, int[]> aufgabe1() {
+        return aufgabe1;
+    }
+
+    public MethodWrapper<T, double[]> aufgabe2() {
+        return aufgabe2;
+    }
+
+    public MethodWrapper<T, String> aufgabe3() {
+        return aufgabe3;
+    }
+
+    public MethodWrapper<T, Integer> aufgabe4() {
+        return aufgabe4;
+    }
+
+    public MethodWrapper<T, Void> aufgabe5() {
+        return aufgabe5;
+    }
+
+    public MethodWrapper<T, int[]> aufgabe6() {
+        return aufgabe6;
+    }
+
+    public MethodWrapper<T, Integer> aufgabe7() {
+        return aufgabe7;
     }
 }
